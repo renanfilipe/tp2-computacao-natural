b30e4595aeca6a93f57275fabe4ec6c7e0c5128e
renan.santos <renan.santos@dcc.ufmg.br>
2018-10-22 10:50:43 -0300

    leitura do arquivo de entrada e construção do grafo

---
+++ main.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+FILE_NAME = "data/graph2.txt"
+
+
+# graph1 = {
+#   'a': {'b': 1, 'c':  4},
+# }
+def build_graph():
+    df = pd.read_csv(FILE_NAME, sep="\t", names=["from", "to", "weight"])
+    graph = dict()
+    for index, row in df.iterrows():
+        try:
+            graph[row["from"]].update({row["to"]: row["weight"]})
+        except KeyError as e:
+            graph[e.args[0]] = {row["to"]: row["weight"]}
+    return graph
+
+
+def main():
+    graph = build_graph()
+
+    print()
+
+
+main()
